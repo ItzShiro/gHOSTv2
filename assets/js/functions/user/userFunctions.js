ed9b11fbb7daa5999ad586bb5b08aaff1a7da59a
ItzShiro <65608540+ItzShiro@users.noreply.github.com>
2021-11-28 11:26:39 +0100

    Rzeczy Dużo

--- assets/js/functions/user/userFunctions.js
+++ assets/js/functions/user/userFunctions.js
@@ -1,16 +1,67 @@
+var popupBody = document.querySelector('.changePopup')
+
 var User = {
     logout: function() {
         firebase.auth().signOut()
     },
     changeDName: function(newName) {
+        popupBody.classList.remove('active')
         firebase.auth().currentUser.updateProfile({
             displayName: newName
         }).then(() => {
             console.log(`Changed displayName to: ${newName}`)
+            Content.notification("Succsess", "Your Username has been changed to " + newName + ", Reloading in 5seconds")
         }).catch((error) => {
             console.log("An Error occured while changing displayName")
+            Content.notification("Error", "An Error occured while changing Nickname")
             console.log(error)
         });
+        window.setTimeout(() => {
+            window.location.reload(true);
+        }, 5000)
+    },
+    changePassword: function(newPassword) {
+        popupBody.classList.remove('active')
+        const user = firebase.auth().currentUser;
+
+        user.updatePassword(newPassword).then(() => {
+            console.log("Password Changed")
+            Content.notification("Succsess", "Your password has been changed")
+        }).catch((error) => {
+            console.log(error.code)
+            Content.notification("Error", "An Error occured while changing Password")
+        });
+        window.setTimeout(() => {
+            window.location.reload(true);
+        }, 5000)
+    },
+    closePopup: function() {
+        popupBody.classList.remove('active')
+    },
+    openPopup: function(thingToChange) {
+        var popupBody = document.querySelector('.changePopup')
+        var popupContent = document.querySelector('.changePopup .content')
+
+        popupBody.classList.add('active')
+
+        switch (thingToChange) {
+            case ("password"):
+                popupContent.innerHTML = `
+                <div class="content">
+                    <span>Change Password</span>
+                    <input type="text">
+                    <button onclick="User.changePassword(document.querySelector('.changePopup .content input').value)">Change</button>
+                </div>`;
+                break;
+            case ("dname"):
+                popupContent.innerHTML = `
+                    <div class="content">
+                        <span>Change Username</span>
+                        <input type="text">
+                        <button onclick="User.changeDName(document.querySelector('.changePopup .content input').value)">Change</button>
+                    </div>`;
+                break;
+        }
     }
 }
 var Content = {
@@ -26,5 +77,22 @@ var Content = {
             document.querySelector('.dropdown .content .settings').style.background = "transparent"
         }, 1000)
         return this;
+    },
+    notification: function(thumbnail, content) {
+        var content = `
+      <div class="notification">
+        <div class="thumbnail">${thumbnail}</div>
+        <div class="content">${content}</div>
+      </div>
+      `;
+        var div = document.createElement("div");
+        div.innerHTML = content;
+        document.querySelector(".notifications").appendChild(div);
+        window.setTimeout(() => {
+            div.classList.add('hide')
+            window.setTimeout(() => {
+                document.querySelector(".notifications").removeChild(div);
+            }, 200);
+        }, 2000);
     }
 }
