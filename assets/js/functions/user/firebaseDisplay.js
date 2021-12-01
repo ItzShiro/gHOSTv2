bc0229431997c0fdd2e55da82371f20408804480
ItzShiro <65608540+ItzShiro@users.noreply.github.com>
2021-12-01 19:19:50 +0100

    ASD
    Deleting Posts worgz

--- assets/js/functions/user/firebaseDisplay.js
+++ assets/js/functions/user/firebaseDisplay.js
@@ -6,15 +6,23 @@ firebase.database().ref("posts").on("child_added", function(snapshot) {
     var html = "";
 
     if (snapshot.val().senderUid == user.uid) {
-        html = `<div id="${snapshot.key}" class="post">
+        html = `<div  id="${snapshot.key}" class="post">
         <div class="thumbnail">
-          <div class="avatar">
-            <img src="${snapshot.val().senderProfile}" alt="profilePic">
-          </div>
-          <div class="text">
-            <span class="nickname">${snapshot.val().sender} <span>You</span></span>
-            <span class="timestamp">${snapshot.val().timestamp}</span>
-          </div>
+            <div class="avatar">
+                <img src="../assets/img/defaultAvatar.png" alt="profilePic">
+            </div>
+            <div class="text">
+                <span class="nickname">${snapshot.val().sender} <span>You</span></span>
+                <span class="timestamp">${snapshot.val().timestamp}</span>
+            </div>
+            <div class="dropdown">
+                <div onclick="deleteMessage('${snapshot.key}')" class="content">
+                    <div class="delete">
+                        <i class="far fa-trash-alt"></i>
+                        <span>Delete</span>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="border"></div>
         <div class="content">
@@ -22,9 +30,9 @@ firebase.database().ref("posts").on("child_added", function(snapshot) {
           <img src="${snapshot.val().postImageUrl}">
           <div class="border"></div>
         </div>
-        </div>`
+    </div>`
     } else {
-        html = `<div  id="${snapshot.key}" class="post">
+        html = `<div data-id="${snapshot.key}" class="post">
         <div class="thumbnail">
           <div class="avatar">
             <img src="${snapshot.val().senderProfile}" alt="profilePic">
@@ -46,3 +54,13 @@ firebase.database().ref("posts").on("child_added", function(snapshot) {
     document.querySelector('.posts .content').insertAdjacentHTML('afterbegin', html);
 
 })
+
+function deleteMessage(self) {
+    firebase.database().ref("posts").child(self).remove();
+}
+firebase.database().ref("posts").on("child_removed", function(snapshot) {
+    document.getElementById(snapshot.key).remove();
+});
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
