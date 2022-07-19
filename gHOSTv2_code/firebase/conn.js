2dbd255307f0a39bf3edcd4a6b14079e11666ea8
ItzShiro <65608540+ItzShiro@users.noreply.github.com>
2022-07-19 05:05:41 +0200

    Update conn.js

--- gHOSTv2_code/firebase/conn.js
+++ gHOSTv2_code/firebase/conn.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCgaLxAMcbXgXqIPHTzFH5vt9SQyPJhAdo",
-    authDomain: "ghost-chat-v2.firebaseapp.com",
-    projectId: "ghost-chat-v2",
-    storageBucket: "ghost-chat-v2.appspot.com",
-    messagingSenderId: "430406752276",
-    appId: "1:430406752276:web:3fafb6706b0149f54e2f01"
+    //Your Firebase Config if you want to make your version of gHOSTv2
 };
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
