0c06f76fc7193384c3f344ae85bc588f69dbb778
CartoonGuy <65608540+ItzShiro@users.noreply.github.com>
2022-09-23 00:22:50 +0200

    Update conn.js

--- gHOSTv2_code/firebase/conn.js
+++ gHOSTv2_code/firebase/conn.js
@@ -1,5 +1,8 @@
 var firebaseConfig = {
     //Your Firebase Config if you want to make your version of gHOSTv2
 };
-firebase.initializeApp(firebaseConfig);
+function init(){
+    if(firebaseConfig == null) return init()
+    firebase.initializeApp(firebaseConfig);
+}
 const auth = firebase.auth();
